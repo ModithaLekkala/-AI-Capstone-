a2f3b549abb0426986e9d0dc51229d29b8e35c3c
Simone Geraci <sgeraci.dev@gmail.com>
2025-05-05 16:01:45 -0500

    Main modules updated
    New and updated pytorch module, consistent result obtained from these configuration

--- pysrc/metrics.py
+++ pysrc/metrics.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay        
-import math
+import numpy as np
 from datetime import datetime
 
 RED = "\033[91m"
@@ -9,18 +9,135 @@ RESET = "\033[0m"
 
 
 class MetricsManager():
-    def __init__(self, case_names, subplots_per_fig=10):
-        self.subplots_per_fig = subplots_per_fig
+    def __init__(self, init_lr, init_epochs, scheduler, hidden_layers):
         self.cases = {}
-        for name in case_names:
-            self.cases[name] = {
-                'cms': [],
-                'cms_names': []
-            }
+        self.bestacc = -np.inf
+        self.bestfold = 0
+        self.init_lr = init_lr
+        self.curr_lr = init_lr
+        self.lr_regions = set()
+        self.epochs = init_epochs
+        self.scheduler = scheduler
+        self.hidden_layers = hidden_layers
+
+    def initCase(self, case):
+        self.cases[case] = {
+            'cms': [],
+            'cms_names': [],
+            'accuracies': [],
+            'losses': []
+        }
+
+    def addLr(self, region: tuple):
+        lr, epoch = region
         
+        if (lr != self.curr_lr):
+            if len(self.lr_regions) == 0:
+                self.lr_regions.add((1, epoch, self.curr_lr))
+            else:
+                _, last_epoch, _ = list(self.lr_regions)[0]
+                self.lr_regions.add((last_epoch, epoch, self.curr_lr))
+            self.curr_lr = lr
+        elif epoch == self.epochs:
+            if len(self.lr_regions) == 0:
+                self.lr_regions.add((1, epoch, self.curr_lr))
+            else:
+                _, last_epoch, _ = list(self.lr_regions)[0]
+                self.lr_regions.add((last_epoch, epoch, self.curr_lr))
+        
+
+    def addAcc(self, case, acc):
+        if case not in self.cases:
+            self.initCase(case)
+        self.cases[case]['accuracies'].append(acc)
+        if acc > self.bestacc:
+            self.bestacc = acc
+            self.bestfold = case[5]
+
+    def addLoss(self, case, loss):
+        if case not in self.cases:
+            self.initCase(case)
+        self.cases[case]['losses'].append(loss)
+
+    def displayTrainEvalAcc(self, model_name, dataset_name, epochs):
+        epochs = list(range(1, epochs+1))
+        plt.figure(figsize=(10, 6))
+
+        for case in self.cases.keys():
+            fold=case[5]
+            accuracies = self.cases[case]['accuracies']
+            linewidth = 0.7
+            marker=''
+            label_train=None
+            label_valid=None
+
+            if self.bestfold in case:
+                linewidth = 3
+                marker='o'
+                label_train=f'Best fold {fold} training accuracy'
+                label_valid=f'Best fold {fold} validation accuracy'
+
+            if 'train' in case:
+                plt.plot(epochs, accuracies, linewidth = linewidth,marker=marker, label=label_train)
+                
+            if 'valid' in case:
+                plt.plot(epochs, accuracies, linewidth = linewidth, linestyle='dashed', marker=marker, label=label_valid)
+
+
+        # lr ragions (PLATEAU scheduler)
+        cmap = plt.get_cmap('Set2')
+
+        y_min, y_max = plt.ylim()
+        regions = list(self.lr_regions)
+
+        for i, (xmin, xmax, lr) in enumerate(regions):
+            color = cmap(i % cmap.N) 
+            plt.axvspan(xmin, xmax, color=color, alpha=0.3)
+            yloc = y_max - 0.5 * (y_max - y_min)
+            xmid = (xmin + xmax) / 2
+            plt.text(xmid, yloc, f'lr:{lr:.0e}',
+                    ha='center', va='center',
+                    fontsize=10,
+                    alpha=0.8)
             
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.ylim(top=1)
+        plt.title(f'Training {model_name.upper()} model \nBalanced dataset: {dataset_name.upper()} \nLr scheduler: {self.scheduler} \nInit_lr: {self.init_lr} \nNeurons: {self.hidden_layers}')
+        plt.legend(loc='lower right')
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig(f'pysrc/metric_plots/training_acc_{model_name}_{dataset_name}_{datetime.now().strftime("%Y%m%d-%H:%M:%S.%f")[:-3]}.png')
+
+    def displayLosses(self, model_name, dataset_name):
+        plt.figure(figsize=(8, 5))
+
+        for case in self.cases.keys():
+            if 'train' in case:
+                fold=case[5]
+                losses = self.cases[case]['losses']
+                x = []
+                y = []
+
+                for (loss, epoch) in losses:
+                    x.append(epoch), y.append(loss)
+
+                plt.plot(x, y, linewidth=0.7)
+                plt.xlabel('Epoch')
+                plt.ylabel('Loss')
+                plt.title(f'{model_name.upper()} {dataset_name.upper()} loss over training time')
+                plt.legend()
+                plt.grid(True)
+                plt.tight_layout()
+                plt.savefig(f'pysrc/metric_plots/training_loss__{model_name}_{dataset_name}_fold{fold}__{datetime.now().strftime("%Y%m%d-%H:%M:%S.%f")[:-3]}.png')
+
+
+
     def addConfMatrix(self, case, y_true, y_pred, title=None):
         cm = confusion_matrix(y_true, y_pred)
+        if case not in self.cases:
+            self.initCase(case)
+
         self.cases[case]['cms'].append(cm)
         if title != None:
             self.cases[case]['cms_names'].append(title)
@@ -28,7 +145,7 @@ class MetricsManager():
             self.cases[case]['cms_names'].append('DEFAULT TITLE')
         
 
-    def displayConfMatrixPlot(self, case, kwargs):
+    def displayConfMatrixPlot(self, case, model_name, dataset_name):
         n_matrices = len(self.cases[case]['cms'])
         rows = 2
         cols = 5
@@ -42,15 +159,15 @@ class MetricsManager():
                 ax.set_title(title)
 
             plt.tight_layout()
-            plt.savefig(f'pysrc/metric_plots/{case}{file_ct}__{kwargs}_{datetime.now().strftime("%Y%m%d-%H:%M:%S.%f")[:-3]}.png')
+            plt.savefig(f'pysrc/metric_plots/confusions/{case}{file_ct}__{model_name}__{dataset_name}_{datetime.now().strftime("%Y%m%d-%H:%M:%S.%f")[:-3]}.png')
             file_ct += 1
             n_matrices -= 10 # each png file contains 10 epoch conf matrices
             self.cases[case]['cms'] = self.cases[case]['cms'][10:]
             self.cases[case]['cms_names'] = self.cases[case]['cms_names'][10:]
 
-        self.cases[case] = None
+        # self.cases[case] = None
 
-    def batchLog(self, mode, epoch_no, curr_len, dataset_len, loss, acc):
+    def batchLog(self, mode, epoch_no, dataset_len, loss, acc, curr_len, kfold, epochs):
         if(acc < 0.7):
             acc_str = self.redLog(acc*100)
         elif(acc > 0.92):
@@ -58,11 +175,12 @@ class MetricsManager():
         else:
             acc_str = f'{acc*100:.3f}%'
 
-        print(f"{mode} | Epoch: [{epoch_no}] [{curr_len:>6d}/{dataset_len:>6d}] Loss: {loss:>5f} Accuracy: {acc_str}")
+        print(f"{mode} | Kfold: [{kfold}] Epoch: [{epoch_no:>3d}\{epochs:>3d}] [{curr_len:>6d}/{dataset_len:>6d}] Loss: {loss:>5f} Accuracy: {acc_str}")
 
     def redLog(self, string):
         return f'{RED}{(string):.3f}%{RESET}'
     
     def greenLog(self, string):
         return f'{GREEN}{(string):.3f}%{RESET}'
+
         
--- pysrc/trainer.py
+++ pysrc/trainer.py
@@ -1,4 +1,5 @@
 import os
+import ast
 import pandas as pd
 
 from utils import get_model_cfg, data_preprocess, data_binarization
@@ -19,16 +20,32 @@ from metrics import MetricsManager
 from torchsampler import ImbalancedDatasetSampler
 
 from models import smaller, deeper
+from sklearn.model_selection import StratifiedShuffleSplit
 
+
+TRAIN = 'train'
+VALID = 'valid'
 
 class Trainer():
     def __init__(self, args):
         self.args = args
 
-        # gather configurations
+        # randomness
+        self.random_seed = 42
+        torch.manual_seed(self.random_seed)
+
+        # init
         self.cfg = get_model_cfg()
         self.num_classes = self.cfg.getint('MODEL', 'NUM_CLASSES')
         self.dataset = self.cfg.get('MODEL', 'DATASET')
+        self.selected_feats = ['sttl', 'ct_srv_dst', 'ct_dst_src_ltm', 'ct_srv_src', 'sbytes', 'smean', 'synack', 'dmean', 'tcprtt', 'ct_src_ltm', 'dbytes', 'service', 'ct_dst_sport_ltm', 'dloss', 'dload', 'ct_dst_ltm', 'sloss', 'swin', 'label']
+        self.model_name = 'bnn' if self.args.quantized else 'full'
+        self.dataset_name = 'balanced' if self.args.balance_dataset else 'vanilla'
+        self.kfolder = StratifiedShuffleSplit(n_splits=self.args.folds, test_size=0.2, random_state=self.random_seed)
+        self.kfold_idx = 0
+        self.best_acc = -np.inf
+        self.metrics_manager = MetricsManager(self.args.lr, self.args.epochs, self.args.scheduler, ast.literal_eval(self.cfg.get('MODEL', 'OUT_FEATURES')))
+        self.reset_stats()
         
         # preprocess dataset and dataloader
         if self.dataset == 'UNSW_NB15':
@@ -44,40 +61,35 @@ class Trainer():
                 ]
             }
 
-            print()
-            print('TRAINING SET PREPROCESSING')
-            X_tr, Y_tr, Xbin_tr, Xbin_desc_tr,_, _ = data_preprocess(data_tr, dict, binarization=self.args.quantized)
-            print()
-            print('TESTING SET PREPROCESSING')
-            X_te, Y_te, Xbin_te, Xbin_desc_te, _, _ = data_preprocess(data_te, dict, binarization=self.args.quantized)
+            data = pd.concat([data_tr, data_te], ignore_index=True)
+            data = data[self.selected_feats]
 
-        train_dataset = self.builder(Xbin_tr if self.args.quantized else X_tr, Y_tr)
-        test_dataset = self.builder(Xbin_te if self.args.quantized else X_te, Y_te)
-        
-        # balancer for unbalanced dataset
-        tr_shuffle = True
-        tr_sampler = None
-        if self.args.balance_dataset:
-            tr_shuffle = False
-            tr_sampler = ImbalancedDatasetSampler(train_dataset)
-
-        self.train_dataloader = DataLoader(train_dataset, batch_size=self.args.batch_size, sampler=tr_sampler, shuffle=tr_shuffle)
-        self.test_dataloader = DataLoader(test_dataset, batch_size=self.args.batch_size, shuffle=False)
-        
-        # set model and evaluation metrics
+            labels=data[data.columns[-1]]
+            print(f'{sum(labels)} samples of class 1')
+            print(f'{len(labels) - sum(labels)} samples of class 0')
+            print('\nDATASETS PREPROCESSING')
+            X_df, self.Y, _ = data_preprocess(data, dict, binarization=self.args.quantized)
+ 
+        Xbin, _ = data_binarization(X_df.astype('int'))
+        self.X = torch.tensor(Xbin, dtype=torch.float32)
+        self.nn_size = self.X.shape[1]
+    
         self.device = torch.device('cpu')
         if self.args.quantized:
-            assert sum(Xbin_desc_tr.values()) == sum(Xbin_desc_te.values())
-            self.nn_size = sum(Xbin_desc_tr.values())
-            self.model = smaller(self.cfg, self.nn_size)
+            self.model_f = smaller
         else:
-            assert X_tr.shape[1] == X_te.shape[1]
-            self.nn_size = X_tr.shape[1]
-            self.model = deeper(self.cfg, self.nn_size)
-        self.model = self.model.to(device=self.device)
+            self.model_f = deeper
 
-        self.epoch = 0
-        self.best_val_acc = 0
+        if self.args.subset_size:
+            N = len(self.Y)
+            S = min(args.subset_size, N)
+            # generate a shuffled permutation of indices [0..N-1]
+            perm = torch.randperm(N, generator=torch.Generator().manual_seed(self.random_seed))
+            # take the first S indices
+            sel = perm[:S]
+            # subset X and Y
+            self.X = self.X[sel]
+            self.Y = self.Y[sel]
 
         # loss
         if args.loss == 'SqrHinge':
@@ -88,88 +100,114 @@ class Trainer():
         else:
             raise ValueError(f"{args.loss} not supported.")
         self.criterion = self.criterion.to(device=self.device)
-
-        # optimizer
-        if args.optim == 'ADAM':
-            self.optimizer = optim.Adam(
-                self.model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-        elif args.optim == 'SGD':
-            self.optimizer = optim.SGD(
-                self.model.parameters(),
-                lr=self.args.lr,
-                momentum=self.args.momentum,
-                weight_decay=self.args.weight_decay)
         
-        # LR scheduler
-        if args.scheduler == 'PLATEAU':
-            self.scheduler = ReduceLROnPlateau(optimizer=self.optimizer, patience=args.patience, verbose=True, min_lr=1e-6)
-        elif args.scheduler == 'FIXED':
-            self.scheduler = None
-        else:
-            raise Exception("Unrecognized scheduler {}".format(self.args.scheduler))
-        
-        self.metrics_manager = MetricsManager(['train', 'test'])
-        self.reset_stats()
-
 
     def train_model(self):
         torch.autograd.set_detect_anomaly(True)
-        best_acc = -np.inf
+    
+        for train_ids, val_ids in self.kfolder.split(self.X, self.Y):
+            print(f'\n{10*"*"} KFOLD {self.kfold_idx} {10*"*"}\n')
 
-        for epoch in range(0, self.args.epochs):
+            self.model = self.model_f(self.cfg, self.nn_size).to(device=self.device)
+            tr_ds = UNSW_NB15_Dataset(self.X[train_ids], self.Y[train_ids])
+            val_ds = UNSW_NB15_Dataset(self.X[val_ids], self.Y[val_ids])
+            self.sampler = ImbalancedDatasetSampler(tr_ds, self.Y[train_ids]) if self.args.balance_dataset else None
+            self.train_dataloader = DataLoader(tr_ds, batch_size=self.args.batch_size, shuffle=True if not self.args.balance_dataset else None, sampler=self.sampler)
+            self.valid_dataloader = DataLoader(val_ds, batch_size=self.args.batch_size, shuffle=False)
+            self.train_case = f'{TRAIN}{self.kfold_idx}'
+            self.valid_case = f'{VALID}{self.kfold_idx}'
+            
+            # optimizer
+            if self.args.optim == 'ADAM':
+                self.optimizer = optim.Adam(self.model.parameters(),  lr=self.args.lr, weight_decay=self.args.weight_decay)
+            elif self.args.optim == 'SGD':
+                self.optimizer = optim.SGD(self.model.parameters(), lr=self.args.lr, momentum=self.args.momentum, weight_decay=self.args.weight_decay)
+            else:
+                raise Exception(f"Unrecognized optimizer {self.args.scheduler}")
+
+            # LR scheduler
+            if self.args.scheduler == 'PLATEAU':
+                self.scheduler = ReduceLROnPlateau(optimizer=self.optimizer, patience=self.args.patience, verbose=True, min_lr=1e-7, factor=0.1)
+            elif self.args.scheduler == 'FIXED':
+                self.scheduler = None
+            else:
+                raise Exception(f"Unrecognized scheduler {self.args.scheduler}")
+            
+            self.train_fold()
+            
+            torch.save(self.model.state_dict(), f'{self.args.checkpoints_path}/bestkfold_{self.kfold_idx}__{self.model_name}_{self.dataset_name}_acc{self.best_acc:.3f}.pth')
+            self.best_acc = -np.inf
+            self.kfold_idx+=1
+            
+            self.metrics_manager.displayConfMatrixPlot(self.train_case, dataset_name=self.dataset_name, model_name=f'{self.model_name}_kfold{self.kfold_idx}')
+            self.metrics_manager.displayConfMatrixPlot(self.valid_case, dataset_name=self.dataset_name, model_name=f'{self.model_name}_kfold{self.kfold_idx}')
+        
+        self.metrics_manager.displayTrainEvalAcc(f'{self.model_name}', self.dataset_name, self.args.epochs)
+        self.metrics_manager.displayLosses(f'{self.model_name}', self.dataset_name)
+
+    def train_fold(self):
+        for epoch in range(1, self.args.epochs+1):
+                
             self.model.train()
             self.criterion.train()
 
-            for batch, (X, Y) in enumerate(self.train_dataloader):
+            accuracies = []
+
+            for batch, (X_tr, Y_tr) in enumerate(self.train_dataloader):
                 if isinstance(self.criterion, SqrHingeLoss):
-                    target = Y.unsqueeze(1)
+                    target = Y_tr.unsqueeze(1)
                     target_onehot = torch.Tensor(target.size(0), 2)
                     target_onehot.fill_(-1)
                     target_onehot.scatter_(1, target, 1)
                     target = target.squeeze()
                     target_var = target_onehot
                 else:
-                    target_var = Y
+                    target_var = Y_tr
 
                 # forward pass
-                pred = self.model(X)
+                pred = self.model(X_tr)
                 loss = self.criterion(pred, target_var)
                 # backpropagation
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
+                if hasattr(self.model, 'clip_weights'):
+                    self.model.clip_weights(-1, 1)
+
                 if self.args.loss == 'SqrHinge':
                     cls = pred.argmax(1).round()
                 elif self.args.loss == 'CrossEntropy':
                     cls = F.softmax(pred, dim=1).argmax(1)
                     
+                acc = (cls == Y_tr).float().mean()
+                accuracies.append(acc.item())
+                self.metrics_manager.addLoss(self.train_case, (loss.item(), epoch))
+
                 if(batch % self.args.log_freq == 0):
-                    loss, current = loss.item(), batch * self.train_dataloader.batch_size + len(X)     
-                    acc = (cls == Y).float().mean()                    
-                    self.metrics_manager.batchLog('Train', epoch, current, len(self.train_dataloader.dataset), loss, acc)
+                    loss, current = loss.item(), batch * self.train_dataloader.batch_size + len(X_tr)     
+                    self.metrics_manager.batchLog(mode='Train', epoch_no=epoch, curr_len=current, dataset_len=len(self.train_dataloader.dataset), loss=loss, acc=acc, kfold=self.kfold_idx, epochs=self.args.epochs)
 
                 self.per_epoch_tr_pred  = torch.concat([self.per_epoch_tr_pred, cls])
-                self.per_epoch_tr_truth = torch.concat([self.per_epoch_tr_truth, Y])
+                self.per_epoch_tr_truth = torch.concat([self.per_epoch_tr_truth, Y_tr])
 
             with torch.no_grad():
                 val_loss, val_acc = self.eval_model(epoch)
-
+            
+            self.metrics_manager.addAcc(f'valid{self.kfold_idx}', val_acc)
+            self.metrics_manager.addAcc(f'train{self.kfold_idx}', np.mean(accuracies))
             # lr decay
             if self.scheduler != None:
                 self.scheduler.step(val_loss)
-            # add epoch confusion matrix to stats 
-            self.metrics_manager.addConfMatrix('train', self.per_epoch_tr_truth, self.per_epoch_tr_pred, f'Epoch n{epoch}')
+                self.metrics_manager.addLr((self.optimizer.param_groups[0]['lr'], epoch))
+            
+            # statistics
+            self.metrics_manager.addConfMatrix(self.train_case, self.per_epoch_tr_truth, self.per_epoch_tr_pred, f'Epoch n{epoch}')
             self.reset_stats()
 
-            if(best_acc < val_acc):
-                best_acc = val_acc
-                model_name = 'bnn' if self.args.quantized else 'full'
-                dataset_name = 'balanced' if self.args.balance_dataset else 'vanilla'
-                torch.save(self.model.state_dict(), f'{self.args.checkpoints_path}/{model_name}_{dataset_name}_acc{val_acc:.3f}.pth')
-
-        self.metrics_manager.displayConfMatrixPlot('train', kwargs='balanced' if self.args.balance_dataset else 'vanilla')
-        self.metrics_manager.displayConfMatrixPlot('test', kwargs='balanced' if self.args.balance_dataset else 'vanilla')
+            if(self.best_acc < val_acc):
+                self.best_acc = val_acc
+                self.best_fold = self.model.state_dict() 
 
 
     def eval_model(self, epoch):
@@ -179,52 +217,46 @@ class Trainer():
         accuracies = []
         losses = []
 
-        for batch, (X, Y) in enumerate(self.test_dataloader):
+        for batch, (X_val, Y_val) in enumerate(self.valid_dataloader):
             
             # for hingeloss only
             if isinstance(self.criterion, SqrHingeLoss):
-                target = Y.unsqueeze(1)
+                target = Y_val.unsqueeze(1)
                 target_onehot = torch.Tensor(target.size(0), 2)
                 target_onehot.fill_(-1)
                 target_onehot.scatter_(1, target, 1)
                 target = target.squeeze()
                 target_var = target_onehot
             else:
-                target_var = Y
+                target_var = Y_val
 
             # compute output
-            pred = self.model(X)
+            pred = self.model(X_val)
             loss = self.criterion(pred, target_var)
 
             # compute loss
-            loss, current = loss.item(), batch * self.train_dataloader.batch_size + len(X)
+            loss, current = loss.item(), batch * self.train_dataloader.batch_size + len(X_val)
             if self.args.loss == 'SqrHinge':
                 cls = pred.argmax(1).round()
-                acc = (cls == Y).float().mean()
+                acc = (cls == Y_val).float().mean()
             elif self.args.loss == 'CrossEntropy':
                 cls = F.softmax(pred, dim=1).argmax(1)
-                acc = (cls == Y).float().mean()
+                acc = (cls == Y_val).float().mean()
             
             accuracies.append(acc)
             losses.append(loss)
-            self.metrics_manager.batchLog('Valid', epoch, current, len(self.test_dataloader.dataset), loss, acc)
             self.per_epoch_te_pred  = torch.concat([self.per_epoch_te_pred, cls])
-            self.per_epoch_te_truth = torch.concat([self.per_epoch_te_truth, Y])
+            self.per_epoch_te_truth = torch.concat([self.per_epoch_te_truth, Y_val])
 
         # add epoch confusion matrix to stats 
-        self.metrics_manager.addConfMatrix('test', self.per_epoch_te_truth, self.per_epoch_te_pred, f'Epoch n{epoch}')
+        self.metrics_manager.addConfMatrix(self.valid_case, self.per_epoch_te_truth, self.per_epoch_te_pred, f'Epoch n{epoch}')
+        self.metrics_manager.batchLog(mode='Valid', epoch_no=epoch, curr_len=current, dataset_len=len(self.valid_dataloader.dataset), loss=np.array(losses).mean(), acc=np.array(accuracies).mean(), epochs=self.args.epochs, kfold=self.kfold_idx)
+        print('\n')
         return np.array(losses).mean(), np.array(accuracies).mean()
-    
+
+
     def reset_stats(self):
         self.per_epoch_tr_pred = torch.tensor([])
         self.per_epoch_te_pred = torch.tensor([])
         self.per_epoch_tr_truth = torch.tensor([])
         self.per_epoch_te_truth = torch.tensor([])
-            
-        
-
-
-        
-
-
-
