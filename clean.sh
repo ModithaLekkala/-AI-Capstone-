70b07d1bfe6dcab61d3efbf43e281a824f11ea03
Simone Geraci <sgeraci.dev@gmail.com>
2025-05-07 09:38:41 -0500

    Updated clean.sh
    Updated in order to maintain the best teacher weight in mdoels/teacher

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 # delete plots results
 find "pysrc/metric_plots" -type f -print -delete
 
-# delete calculated weights
-find "pysrc/models" -type f -print -delete
+# delete calculated weights except anything under pysrc/models/teacher/
+find "pysrc/models" -type f ! -path "pysrc/models/teacher/*" -print -delete
